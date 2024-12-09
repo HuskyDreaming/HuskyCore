0566f7feba0c5586ef74fea41618fb5509b4c939
HuskyDreaming <huskydevelopment@gmail.com>
2024-12-09 16:19:07 +1300

    1.7.6-SNAPSHOT
    - Fixed sub command Permissions

--- src/main/java/com/huskydreaming/huskycore/commands/abstraction/AbstractCommand.java
+++ src/main/java/com/huskydreaming/huskycore/commands/abstraction/AbstractCommand.java
@@ -35,25 +35,26 @@ public abstract class AbstractCommand implements CommandExecutor, Command, TabCo
                 return true;
             }
 
-            if(commandSender instanceof ConsoleCommandSender consoleCommandSender) {
-                if(subCommand instanceof ConsoleCommandProvider consoleCommandProvider) {
+            if (!(commandSender.hasPermission(getLabel() + "." + subCommand.getLabel()) && commandSender.isOp())) {
+                commandSender.sendMessage(getPermission().prefix());
+                return true;
+            }
+
+            if (commandSender instanceof ConsoleCommandSender consoleCommandSender) {
+                if (subCommand instanceof ConsoleCommandProvider consoleCommandProvider) {
                     consoleCommandProvider.onCommand(consoleCommandSender, strings);
                     return true;
                 }
             }
 
-            if(commandSender instanceof Player player) {
-                if (!(player.hasPermission(getLabel() + "." + subCommand.getLabel().toLowerCase()) || player.isOp())) {
-                    commandSender.sendMessage(getPermission().prefix());
-                    return true;
-                }
-
+            if (commandSender instanceof Player player) {
                 if (subCommand instanceof PlayerCommandProvider playerCommandProvider) {
                     playerCommandProvider.onCommand(player, strings);
                     return true;
                 }
             }
-            if(subCommand instanceof CommandProvider commandProvider) {
+
+            if (subCommand instanceof CommandProvider commandProvider) {
                 commandProvider.onCommand(commandSender, strings);
                 return true;
             }
@@ -65,24 +66,26 @@ public abstract class AbstractCommand implements CommandExecutor, Command, TabCo
 
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull org.bukkit.command.Command command, @NotNull String s, @NotNull String[] strings) {
-        if (strings.length == 1) return commandHandler.getCommands().stream().map(c -> c.getLabel().toLowerCase()).collect(Collectors.toList());
-        if(strings.length > 1) {
+        if (strings.length == 1) {
+            return commandHandler.getCommands().stream().map(c -> c.getLabel().toLowerCase()).collect(Collectors.toList());
+        }
+        if (strings.length > 1) {
             Command subCommand = commandHandler.getCommand(strings[0].toLowerCase());
             if (subCommand != null) {
 
-                if(commandSender instanceof ConsoleCommandSender consoleCommandSender) {
-                    if(subCommand instanceof ConsoleCommandProvider consoleCommandProvider) {
+                if (commandSender instanceof ConsoleCommandSender consoleCommandSender) {
+                    if (subCommand instanceof ConsoleCommandProvider consoleCommandProvider) {
                         return consoleCommandProvider.onTabComplete(consoleCommandSender, strings);
                     }
                 }
 
-                if(commandSender instanceof Player player) {
-                    if(subCommand instanceof PlayerCommandProvider playerCommandProvider) {
+                if (commandSender instanceof Player player) {
+                    if (subCommand instanceof PlayerCommandProvider playerCommandProvider) {
                         return playerCommandProvider.onTabComplete(player, strings);
                     }
                 }
 
-                if(subCommand instanceof CommandProvider commandProvider) {
+                if (subCommand instanceof CommandProvider commandProvider) {
                     return commandProvider.onTabComplete(commandSender, strings);
                 }
             }
@@ -93,5 +96,6 @@ public abstract class AbstractCommand implements CommandExecutor, Command, TabCo
     public abstract void onCommand(CommandSender commandSender, String[] strings);
 
     public abstract Parseable getUsage();
+
     public abstract Parseable getPermission();
 }
